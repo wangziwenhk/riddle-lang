9747b39f4919a5421749753aeb1ddc9344908147
wangziwenhk <wangziwen2011@icloud.com>
2025-02-18 22:49:21 +0800

    修复了FuncCall的段错误问题

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,20 @@ import Support.BuildQueue;
 import Support.Options;
 using namespace std;
 int main(const int argc, char *argv[]) {
-    std::setlocale(LC_ALL, "en_US.UTF-8");
-    // 交由 Options 进行处理
-    // Parser
-    Riddle::BuildQueue buildQueue;
-    for(int i = 1; i < argc; i++) {
-        const auto option = Riddle::Option(argv[i]);
-        buildQueue.parserFile(option);
+    try {
+        std::setlocale(LC_ALL, "en_US.UTF-8");
+        // 交由 Options 进行处理
+        // Parser
+        Riddle::BuildQueue buildQueue;
+        for(int i = 1; i < argc; i++) {
+            const auto option = Riddle::Option(argv[i]);
+            buildQueue.parserFile(option);
+        }
+        buildQueue.start();
     }
-    buildQueue.start();
+    catch(const std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
     return 0;
 }
